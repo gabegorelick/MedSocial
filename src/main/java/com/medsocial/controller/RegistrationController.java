8850810d1d24897bf3305dad9532aafd67525043
Gabe Gorelick <gabegorelick@gmail.com>
2011-11-05 17:35:41 -0400

    Fix registration bug

--- src/main/java/com/medsocial/controller/RegistrationController.java
+++ src/main/java/com/medsocial/controller/RegistrationController.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ExtendedModelMap;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,10 +33,10 @@ public class RegistrationController {
 	private UserRegistry registry;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public Model registrationForm() {
-		Model model = new ExtendedModelMap();
-		model.addAttribute(new RegistrationForm());
-		return model;
+	public ModelAndView registrationForm() {
+		ModelAndView mav = new ModelAndView("newUser/register");
+		mav.addObject(new RegistrationForm());
+		return mav;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
