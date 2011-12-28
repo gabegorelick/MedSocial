fbad98311ed323b82ecc27c7c6c50b9f7239bfc4
Gabe Gorelick <gabegorelick@gmail.com>
2011-12-28 03:21:01 -0500

    Turn off Spring Security and turn on mock user credentials

--- src/main/java/com/medsocial/web/UserHandlerInterceptor.java
+++ src/main/java/com/medsocial/web/UserHandlerInterceptor.java
@@ -1,24 +1,36 @@
 package com.medsocial.web;
 
+import java.util.EnumSet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.medsocial.model.GaeUser;
+import com.medsocial.security.AppRole;
 
 public class UserHandlerInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		Object principal = auth.getPrincipal();
-		if (principal instanceof GaeUser && modelAndView != null) {
-			GaeUser user = (GaeUser) auth.getPrincipal();
-			modelAndView.addObject("user", user);
-		}
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		Object principal = auth.getPrincipal();
+//		if (principal instanceof GaeUser && modelAndView != null) {
+//			GaeUser user = (GaeUser) auth.getPrincipal();
+//			modelAndView.addObject("user", user);
+//		}
+		
+		// TODO get real user credentials
+		GaeUser user = new GaeUser();
+		user.setAuthorities(EnumSet.of(AppRole.DOCTOR, AppRole.PATIENT));
+		user.setForename("Jon");
+		user.setSurname("Everyman");
+		// matches an old user from the datastore
+		user.setUserId("18580476422013912411");
+		user.setEmail("jon@example.com");
+		user.setEnabled(true);
+		modelAndView.addObject("user", user);
 	}
 }
